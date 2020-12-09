fc96a1e77ebc8b47dcf95520c87da7e41c1ed383
giuliof <giulio@glgprograms.it>
2020-12-09 15:14:36 +0100

    Added support for atxmega16a4u

--- src/ioreg.h
+++ src/ioreg.h
@@ -108,5 +108,6 @@ extern gdb_io_reg_def_type attiny48_io_registers[];
 extern gdb_io_reg_def_type attiny88_io_registers[];
 extern gdb_io_reg_def_type atmega128rfa1_io_registers[];
 extern gdb_io_reg_def_type atmega256rfr2_io_registers[];
+extern gdb_io_reg_def_type atxmega16a4u_io_registers[];
 
 #endif /* INCLUDE_IOREG_H */
