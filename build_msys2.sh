59a6a6d743e54344d01623e1128973ecaf0d8362
Florin Popescu <florinsgpopescu@gmail.com>
2021-02-12 04:58:01 +0200

    added dependencies

--- build_msys2.sh
+++ build_msys2.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+#These libraries must be specially built under msys2
+git clone https://github.com/libusb/libusb.git
+cd ./libusb
+./bootstrap.sh
+./configure
+make -j $NUMBER_OF_PROCESSORS
+make install
+cd ..
+
+git clone https://github.com/Florin-Popescu/libusb-compat-0.1.git
+cd ./libusb-compat-0.1
+./bootstrap.sh
+./configure
+make -j $NUMBER_OF_PROCESSORS
+make install
+cd ..
+
+git clone https://github.com/Florin-Popescu/hidapi.git
+cd ./hidapi
+./bootstrap
+./configure
+make all -j $NUMBER_OF_PROCESSORS
+make install
+cd ..
+
+
 INSTALL_DIR=/c/avr
 
 ./Bootstrap
