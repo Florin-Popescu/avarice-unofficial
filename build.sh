714cfa9fafe49108e72e3c50e37ddda6f962776a
Florin Popescu <florinsgpopescu@gmail.com>
2021-10-07 01:02:25 +0300

    updated linux compatibility

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-case $(uname -o) in
-	Msys)
+case $(uname -o | cut -d '/' -f2) in
+	"Msys")
 		wget https://github.com/Florin-Popescu/libusb-compat-0.1/releases/download/v0.1.7/libusb-compat-0.1_msys_x86_64.zip -O libusb-compat-0.1_msys_x86_64.zip
 		unzip libusb-compat-0.1_msys_x86_64.zip -o
 		cp libusb-compat-0.1_msys_x86_64/* / -r
@@ -22,9 +22,12 @@ case $(uname -o) in
 		make all -j$NUMBER_OF_PROCESSORS
 		make install
 		cd ..
+
+		SUDO=
 		;;
-	Linux)
-		$NUMBER_OF_PROCESSORS=$(nproc)
+	"Linux")
+		NUMBER_OF_PROCESSORS=$(nproc)
+		SUDO=sudo
 		;;
 esac
 
@@ -32,4 +35,4 @@ esac
 ./configure
 
 make all -j$NUMBER_OF_PROCESSORS
-make install
+$SUDO make install
--- package.sh
+++ package.sh
@@ -2,12 +2,19 @@
 
 TOOL=avarice
 ARCH=$(uname -m)
-OS=$(uname -o | tr '[:upper:]' '[:lower:]')
+OS=$(uname -o | cut -d '/' -f2 | tr '[:upper:]' '[:lower:]')
 PACKAGE="$TOOL"_"$OS"_"$ARCH"
 export DESTDIR="$(pwd)/""$PACKAGE"
 
 mkdir $DESTDIR
 make install
 
-cd $DESTDIR
-zip -r ../$PACKAGE.zip *
+case $(uname -o | cut -d '/' -f2) in
+	"Msys")
+		cd $DESTDIR
+		zip -r ../$PACKAGE.zip *
+		;;
+	"Linux")
+		dpkg-deb --build --root-owner-group $PACKAGE
+		;;
+esac
