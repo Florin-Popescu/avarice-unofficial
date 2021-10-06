55467f41b36d3532e2d788b6738de18bff5b89bf
Florin Popescu <florinsgpopescu@gmail.com>
2021-10-06 23:52:05 +0300

    use prebuilt libusb-compat-0.1

--- build_msys2.sh
+++ build_msys2.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#These libraries must be specially built under msys2
-git clone https://github.com/libusb/libusb.git
-cd ./libusb
-./bootstrap.sh
-./configure
-make -j $NUMBER_OF_PROCESSORS
-make install
-cd ..
+wget https://github.com/Florin-Popescu/libusb-compat-0.1/releases/download/v0.1.7/libusb-compat-0.1_msys2_x86_64.zip
+unzip libusb-compat-0.1_msys2_x86_64.zip
+cp libusb-compat-0.1_msys2_x86_64/* / -r
 
-git clone https://github.com/Florin-Popescu/libusb-compat-0.1.git
-cd ./libusb-compat-0.1
+#These libraries are not available pre-built under msys2
+git clone git@github.com:libusb/libusb.git
+cd ./libusb
 ./bootstrap.sh
 ./configure
 make -j $NUMBER_OF_PROCESSORS
