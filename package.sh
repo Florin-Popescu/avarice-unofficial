08c95ff77ff8642073046e81f2343313f642c721
Florin Popescu <florinsgpopescu@gmail.com>
2022-09-26 02:09:48 +0200

    added support for arch builds

--- package.sh
+++ package.sh
@@ -3,18 +3,24 @@
 TOOL=avarice
 ARCH=$(uname -m)
 OS=$(uname -o | cut -d '/' -f2 | tr '[:upper:]' '[:lower:]')
-PACKAGE="$TOOL"_"$OS"_"$ARCH"
-export DESTDIR="$(pwd)/""$PACKAGE"
+UPSTREAM_VERSION=2.14
 
+if [ -x "$(command -v dpkg-deb)" ]; then
+	DISTRO=deb
+elif [ -x "$(command -v makepkg)" ]; then
+	DISTRO=arch
+fi
+
+PACKAGE="$TOOL"_"$OS"_"$DISTRO"_"$ARCH"
+DESTDIR=./"$PACKAGE"
+
+
+if [ $DISTRO == deb ]; then
 mkdir $DESTDIR
-make install
-
-case $(uname -o | cut -d '/' -f2) in
-	"Msys")
-		cd $DESTDIR
-		zip -r ../$PACKAGE.zip *
-		;;
-	"Linux")
-		dpkg-deb --build --root-owner-group $PACKAGE
-		;;
-esac
+	make install
+	dpkg-deb --build --root-owner-group $PACKAGE
+elif [ $DISTRO == arch ]; then
+	tar --create --gzip --file="$PACKAGE"/"$TOOL"-"$UPSTREAM_VERSION".tar.gz .
+	cd $PACKAGE
+	makepkg
+fi
