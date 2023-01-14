b8c67ae3dc95f2321e122c5f9da6f54ad91bcf60
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2023-01-14 23:51:11 +0100

    Correct build script
    Changes to libusb-compat-0.1 have been merged

--- build.sh
+++ build.sh
@@ -1,34 +1,39 @@
+  GNU nano 7.1                                                                              build.sh
 #!/bin/bash
 
 case $(uname -o | cut -d '/' -f2) in
-	"Msys")
-		wget https://github.com/Florin-Popescu/libusb-compat-0.1/releases/download/v0.1.7/libusb-compat-0.1_msys_x86_64.zip -O libusb-compat-0.1_msys_x86_64.zip
-		unzip libusb-compat-0.1_msys_x86_64.zip -o
-		cp libusb-compat-0.1_msys_x86_64/* / -r
+        "Msys")
+                #These libraries are not available pre-built under msys2
+                git clone https://github.com/libusb/libusb-compat-0.1.git
+                cd ./libusb-compat-0.1
+                ./bootstrap.sh
+                ./configure
+                make -j$NUMBER_OF_PROCESSORS
+                make install
+                cd ..
 
-		#These libraries are not available pre-built under msys2
-		git clone git@github.com:libusb/libusb.git
-		cd ./libusb
-		./bootstrap.sh
-		./configure
-		make -j$NUMBER_OF_PROCESSORS
-		make install
-		cd ..
+                git clone https://github.com/libusb/libusb.git
+                cd ./libusb
+                ./bootstrap.sh
+                ./configure
+                make -j$NUMBER_OF_PROCESSORS
+                make install
+                cd ..
 
-		git clone git@github.com:libusb/hidapi.git
-		cd ./hidapi
-		./bootstrap
-		./configure
-		make all -j$NUMBER_OF_PROCESSORS
-		make install
-		cd ..
+                git clone https://github.com/libusb/hidapi.git
+                cd ./hidapi
+                ./bootstrap
+                ./configure
+                make all -j$NUMBER_OF_PROCESSORS
+                make install
+                cd ..
 
-		SUDO=
-		;;
-	"Linux")
-		NUMBER_OF_PROCESSORS=$(nproc)
-		SUDO=sudo
-		;;
+                SUDO=
+                ;;
+        "Linux")
+                NUMBER_OF_PROCESSORS=$(nproc)
+                SUDO=sudo
+                ;;
 esac
 
 ./Bootstrap
